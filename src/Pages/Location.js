8af66a1d7c9c3e20af4f518eba578ab75aec7071
AidanaTur7777 <turdaeva.002@gmail.com>
2022-10-02 23:01:27 +0600

    first commit

---
+++ src/Pages/Location.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card/Card";
+import InputGroup from "../components/Filter/category/InputGroup";
+
+const Location = () => {
+  let [results, setResults] = React.useState([]);
+  let [info, setInfo] = useState([]);
+  let { dimension, type, name } = info;
+  let [number, setNumber] = useState(1);
+
+  let api = `https://rickandmortyapi.com/api/location/${number}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+
+      let a = await Promise.all(
+        data.residents.map((x) => {
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
+    })();
+  }, [api]);
+
+  return (
+    <div className="container">
+      <div className="row mb-3">
+        <h1 className="text-center mb-3">
+          Расположение :
+          <span className="text-primary">
+            {name === "" ? "Unknown" : name}
+          </span>
+        </h1>
+        <h5 className="text-center">
+          Измерение: {dimension === "" ? "Unknown" : dimension}
+        </h5>
+        <h6 className="text-center">Тип: {type === "" ? "Unknown" : type}</h6>
+      </div>
+      <div className="row">
+        <div className="col-lg-3 col-12 mb-4">
+          <h4 className="text-center mb-4">Расположение Рика</h4>
+          <InputGroup name="Location" changeID={setNumber} total={126} />
+        </div>
+        <div className="col-lg-8 col-12">
+          <div className="row">
+            <Card page="/location/" results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
